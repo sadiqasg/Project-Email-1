3264a8d758dfc066eb4b04a58a7d935a98f2ef5a
Abubakar Sambo <sadiqasg@gmail.com>
2018-04-12 00:46:45 +0100

    first temp

---
+++ email.php
@@ -0,0 +1,108 @@
+<!DOCTYPE>
+<html xmlns="http://www.w3.org/1999/xhtml">
+ <head>
+  <meta name="viewport" width="device-width, initial-scale=1.0" charset="utf-8">
+  <title>HNG Project-Email</title>
+</head>
+
+<body style="margin: 0; padding: 0;">
+ <table border="1" cellpadding="0" cellspacing="0" width="100%">
+<tr>
+ <td>
+   <table align="center" border="1px solid #cccccc;" cellpadding="0" cellspacing="0" width="600" style="border-collapse: collapse;">
+    <tr>
+      <td align="center" bgcolor="#70bbd9" style="padding: 40px 0 30px 0; padding: 20px 0 30px 0;">
+        <img src="email.png" alt="banner" width="300" height="230" style="display: block;">
+      </td>
+    </tr>
+    <tr>
+      <td bgcolor="#ffffff" style="padding: 40px 30px 40px 30px;">
+        <table border="0" cellpadding="0" cellspacing="0" width="100%">
+          <tr>
+            <td style="color: #153643; font-family: Arial, sans-serif; font-size: 24px;">
+             <b>Lorem ipsum dolor sit amet!</b>
+            </td>
+          </tr>
+          <tr>
+            <td style="padding: 20px 0px 20px 0px; color: #153643; font-family: Arial, sans-serif; font-size: 16px; line-height: 20px;">
+               Lorem ipsum dolor sit amet, consectetur adipiscing elit. In tempus adipiscing felis, sit amet blandit ipsum volutpat sed. Morbi porttitor, eget accumsan dictum, nisi libero ultricies ipsum, in posuere mauris neque at erat.
+            </td>
+          </tr>
+          <tr>
+            <td>
+              <table border="0" cellpadding="0" cellspacing="0" width="100%">
+               <tr>
+                <td width="260" valign="top">
+         <table border="0" cellpadding="0" cellspacing="0" width="100%">
+            <tr>
+              <td>
+                <img src="pexelsimg2.jpeg" alt="just a pic" width="100%" height="140" style="display: block;" />
+              </td>
+            </tr>
+          <tr>
+            <td style="padding: 25px 0 0 0; color: #153643; font-family: Arial, sans-serif; font-size: 16px; line-height: 20px;">
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. In tempus adipiscing felis, sit amet blandit ipsum volutpat sed. Morbi porttitor, eget accumsan dictum, nisi libero ultricies ipsum, in posuere mauris neque at erat.
+            </td>
+          </tr>
+         </table>
+      </td>
+      <td style="font-size: 0; line-height: 0;" width="20">
+      &nbsp;
+      </td>
+      <td width="260" valign="top">
+       <table border="0" cellpadding="0" cellspacing="0" width="100%">
+        <tr>
+         <td>
+          <img src="pexelsimg0.jpg" alt="just some pic" width="100%" height="140" style="display: block;" />
+         </td>
+        </tr>
+        <tr>
+         <td style="padding: 25px 0 0 0; color: #153643; font-family: Arial, sans-serif; font-size: 16px; line-height: 20px;">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. In tempus adipiscing felis, sit amet blandit ipsum volutpat sed. Morbi porttitor, eget accumsan dictum, nisi libero ultricies ipsum, in posuere mauris neque at erat.
+         </td>
+        </tr>
+       </table>
+      </td>
+     </tr>
+    </table>
+
+    </td>
+  </tr>
+  </table>
+  </td>
+  </tr>
+    <tr>
+      <td bgcolor="#ee4c50" style="padding: 30px 30px 30px 30px;">
+        <table border="0" cellpadding="0" cellspacing="0" width="100%">
+          <tr>
+            <td style="color: #ffffff; font-family: Arial, sans-serif; font-size: 14px; ">
+              &reg; Someone, somewhere 2013<br/>
+                <a href="#" style="color: #ffffff;"><font color="#ffffff">Unsubscribe</font></a> to this newsletter instantly
+            </td>
+            <td align="right">
+              <table border="0" cellpadding="0" cellspacing="0">
+                <tr>
+                  <td>
+                    <a href="http://www.twitter.com/">
+                      <img src="tweet.png" alt="Twitter" width="38" height="38" style="display: block;" border="0" />
+                    </a>
+                  </td>
+                  <td style="font-size: 0; line-height: 0;" width="20">&nbsp;</td>
+                    <td>
+                      <a href="http://www.twitter.com/">
+                        <img src="facebook.png" alt="Facebook" width="38" height="38" style="display: block;" border="0" />
+                      </a>
+                    </td>
+                  </tr>
+                </table>
+              </td>
+            </tr>
+          </table>
+        </td>
+      </tr>
+</table>
+</td>
+</tr>
+</table>
+</body>
+</html>
